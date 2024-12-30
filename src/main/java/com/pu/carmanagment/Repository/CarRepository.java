41dd8273d49d2b90ae6d00b88c4b48ecc46deace
Todor <Todor@DESKTOP-3L49MCF>
2024-12-30 19:11:13 +0200

    created GarageDailyAvailabilityReport method

--- src/main/java/com/pu/carmanagment/Repository/CarRepository.java
+++ src/main/java/com/pu/carmanagment/Repository/CarRepository.java
@@ -2,9 +2,23 @@ package com.pu.carmanagment.Repository;
 
 import com.pu.carmanagment.Entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer> {
+
+    List<Car> findByMake(String make);
+
+    @Query("SELECT c FROM Car c JOIN c.garages g WHERE g.id = :garageId")
+    List<Car> findByGarageId(@Param("garageId") Integer garageId);
+
+
+    @Query("SELECT c FROM Car c WHERE c.productionYear BETWEEN :startYear AND :endYear")
+    List<Car> findByProductionYearBetween(@Param("startYear") LocalDate startYear, @Param("endYear") LocalDate endYear);
 
 }
--- src/main/java/com/pu/carmanagment/Repository/GarageRepository.java
+++ src/main/java/com/pu/carmanagment/Repository/GarageRepository.java
@@ -2,9 +2,16 @@ package com.pu.carmanagment.Repository;
 
 import com.pu.carmanagment.Entity.Garage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GarageRepository extends JpaRepository<Garage,Integer> {
+
+    @Query("SELECT g FROM Garage g WHERE g.city = :city")
+    List<Garage> findByCity(@Param("city") String city);
 
 }
